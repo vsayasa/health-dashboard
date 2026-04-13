bbe34e4a89f1784db374238164e3ba0a8a44de27
Vonn Sayasa <vonnsayas@gmail.com>
2026-04-12 23:25:01 -0400

    initial setup of DB connection logic: make sure to have environment variables from here on

--- app/backend/server.ts
+++ app/backend/server.ts
@@ -4,7 +4,8 @@ import exerciseRouter from "./routes/exercise";
 import nutritionRouter from "./routes/nutrition";
 import wellnessRouter from "./routes/wellness";
 import goalsRouter from "./routes/goals";
-
+import { CosmosClient } from '@azure/cosmos';
+import { BlobServiceClient } from '@azure/storage-blob';
 const app = express();
 app.use(express.json());
 
@@ -20,3 +21,33 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3001, () => console.log("API running on http://localhost:3001"));
+
+
+// DB connection stuff here
+const cosmosConnection = import.meta.env.VITE_CosmosDBConnection;
+
+const client = new CosmosClient(cosmosConnection);
+const database = client.database("metricsdb");
+const usersContainer = database.container("Users"); // partition key is /Users
+const metricsContainer = database.container("Metrics"); // partition key is /Metrics
+
+const storageConnection = import.meta.env.VITE_StorageConnection;
+
+let blobServiceClient = null;
+let imagesContainer = null;
+
+if (storageConnection) {
+  blobServiceClient = BlobServiceClient.fromConnectionString(storageConnection);
+  
+  // Make sure its made
+  imagesContainer = blobServiceClient.getContainerClient("images");
+}
+else {
+  console.log("ur storage acc isnt made correctly");
+}
+export { 
+  client,
+  database,
+  usersContainer,
+  metricsContainer
+}
